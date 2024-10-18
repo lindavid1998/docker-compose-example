536642a49256779bf3c7f4331da9ae786f89340a
David Lin <lindavid1998@gmail.com>
2024-10-17 23:34:56 -0700

    Set up

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState(null)
+  
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000');
+      const data = await response.json()
+      if (response.ok) {
+        setMessage(`${data.year}: ${data.brand} ${data.model}`);
+      } else {
+        setMessage(data.error);
+      }
+    } catch (error) {
+      setMessage(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <p>{message}</p>
+    </>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const pgp = require('pg-promise')(/* options */);
+const db = pgp(
+	`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json()); // enables req.body
+
+app.get('/', async (req, res) => {
+	try {
+		const data = await db.one('SELECT * FROM cars');
+		res.json(data)
+	} catch (error) {
+		res.json({ error });
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
