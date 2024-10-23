a8c288cb56c2540f7417a3517a53396df24c4c07
David Lin <lindavid1998@gmail.com>
2024-10-23 16:17:38 -0700

    Add postgres db to docker-compose with volume

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const pgp = require('pg-promise')(/* options */);
+const pgp = require('pg-promise')(/* options */);
 // const db = pgp(
 // 	`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 // );
+const db = pgp(`postgres://postgres:password@db:5432/postgres`);
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +17,9 @@ app.use(express.json()); // enables req.body
 
 app.get('/', async (req, res) => {
 	try {
-		// const data = await db.one('SELECT * FROM cars');
-    // res.json(data)
-    res.json({ message: 'hello from express'})
+		const data = await db.one('SELECT * FROM cars');
+    res.json(data)
+    // res.json({ message: 'hello from express'})
 	} catch (error) {
 		res.json({ error });
 	}
